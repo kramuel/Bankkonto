127807fb2f741ff444fbdd19150cf2044d57c2e7
Samuel Karlström <sam.karlstrm@gmail.com>
2020-10-23 15:30:20 +0200

    PrintMenu och UserInput test

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bankkonto
 {
@@ -14,17 +16,55 @@ namespace Bankkonto
             //kontot.Hen ska även kunna välja att se saldot på kontot eller avsluta programmet.
 
             //Startmeny metod/loop
+            while (true)
+            {
+
                 //Print Hej välkommen till din bank, #skapa konto, #skriv in ditt bankkonto 
+                PrintMenu();
 
-
-
-            //läs in input func /quitmetod
+                //läs in input func /quitmetod
+                UserInput(0, 3);
 
                 //skapa konto metod
 
                 ////#visa saldo, #sätt in /ta ut pengar #gå ut för se startmeny
 
                 //
+
+            }
+        }
+        static void PrintMenu()
+        {
+            Console.WriteLine("**Välkommen til BANKEN**");
+            Console.WriteLine("1:  Skapa nytt bankkonto.");
+            Console.WriteLine("2:  Logga in på ditt konto.");
+            Console.WriteLine("0:  Avsluta BANKEN.");
+
+        }
+
+        static int UserInput(int start, int count)
+        {
+
+            var menuChoice = Enumerable.Range(start, count);
+            Console.Write("menuChoice = ");
+            foreach (int i in menuChoice)
+                Console.Write(" {0} ",i);
+
+
+
+            int userInput = int.Parse(Console.ReadLine());
+
+
+            //input can only be 0-4
+            while (!menuChoice.Contains(userInput))
+            {
+                //wrong input loop
+                Console.WriteLine("wrong input try again: ");
+                userInput = int.Parse(Console.ReadLine());
+            }
+            return userInput;
+            
+
         }
     }
 }
