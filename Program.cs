ef55b29ee0cabb97d2d6ca77efdd609f5beefc11
Samuel Karlström <sam.karlstrm@gmail.com>
2020-10-23 14:53:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bankkonto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Informationens som ska läsas in är namn, adress, telefonnummer och kontonummer. Informationen
+            //ska lagras i variabler med lämpliga datatyper och namn.
+
+            //Programmet ska visa en meny där användaren kan välja om hen vill sätta in eller ta ut pengar på
+            //kontot.Hen ska även kunna välja att se saldot på kontot eller avsluta programmet.
+
+            //Startmeny metod/loop
+                //Print Hej välkommen till din bank, #skapa konto, #skriv in ditt bankkonto 
+
+
+
+            //läs in input func /quitmetod
+
+                //skapa konto metod
+
+                ////#visa saldo, #sätt in /ta ut pengar #gå ut för se startmeny
+
+                //
+        }
+    }
+}
